7ca41bf28f6f30215ef62865c504c2d31aae32f1
Rohan Baidya <baidya.rohan@gmail.com>
2023-10-26 18:05:56 +0545

    Make basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 
   # def initialize(attributes = {})
   #   # @name  = attributes[:name]
