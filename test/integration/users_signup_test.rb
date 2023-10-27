33e95f2b9b5422c0a952322cd4dc9267abc3b37b
Rohan Baidya <baidya.rohan@gmail.com>
2023-10-27 18:04:17 +0545

    Sign Up Redirect Complete

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,5 +18,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
+    # assert_select 'div#<CSS id for error explanation>'
+    # assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: 
+      { user: 
+        { 
+          name:  "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        } 
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
